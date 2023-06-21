30597fd188b7a154c91e16af93dfa374a2a5e499
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2023-06-21 13:14:55 +0200

    fix syntax for darwin clean podman extension

--- test/e2e/app/podman-extension/podman-extesion_darwin.go
+++ test/e2e/app/podman-extension/podman-extesion_darwin.go
@@ -6,21 +6,23 @@ import (
 )
 
 func cleanup() error {
-	rmConfigContainers := "rm -rf $HOME/.config/containers"
-	// cmd := exec.Command("rm", "-rf", "$HOME\\.config\\containers")
-	cmd := exec.Command(rmConfigContainers...)
+	// rmConfigContainers := "rm -rf $HOME/.config/containers"
+	// cmd := exec.Command(rmConfigContainers...)
+	cmd := exec.Command("rm", "-rf", "$HOME/.config/containers")
 	err := cmd.Start()
 	if err != nil {
 		return err
 	}
-	rmShareContainers := "rm -rf $HOME/.local/share/containers"
-	cmd = exec.Command(rmShareContainers...)
+	// rmShareContainers := "rm -rf $HOME/.local/share/containers"
+	// cmd = exec.Command(rmShareContainers...)
+	cmd = exec.Command("rm", "-rf", "$HOME/.local/share/containers")
 	err = cmd.Start()
 	if err != nil {
 		return err
 	}
-	rmPodman := "sudo rm -rf /opt/podman"
-	cmd = exec.Command(rmPodman...)
+	// rmPodman := "sudo rm -rf /opt/podman"
+	// cmd = exec.Command(rmPodman...)
+	cmd = exec.Command("sudo", "rm", "-rf", "/opt/podman")
 	return cmd.Start()
 }
 
