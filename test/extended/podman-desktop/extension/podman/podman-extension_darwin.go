cebdc5467c1b57e57cdecda4f520baa37010e84d
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2024-02-09 09:59:18 +0100

    chore: ensure no previous instance of podman desktop is running on target host
    Signed-off-by: Adrian Riobo Lorenzo <ariobolo@redhat.com>

--- test/extended/podman-desktop/extension/podman/podman-extension_darwin.go
+++ test/extended/podman-desktop/extension/podman/podman-extension_darwin.go
@@ -15,6 +15,8 @@ const (
 	installerAgree    = "Agree"
 	installerInstall  = "Install"
 	installerClose    = "Close"
+
+	selectLocationTitle = "Select a Destination"
 )
 
 func cleanupSystem() error {
@@ -46,6 +48,15 @@ func runInstaller(userPassword string) error {
 	}
 	if err := i.Click(installerAgree, delay.SMALL); err != nil {
 		return installerError(err)
+	}
+	selectLocationExists, err := i.ExistsWithType(selectLocationTitle, "text")
+	if err != nil {
+		return installerError(err)
+	}
+	if selectLocationExists {
+		if err := i.Click(installerContinue, delay.SMALL); err != nil {
+			return installerError(err)
+		}
 	}
 	if err := i.Click(installerInstall, delay.SMALL); err != nil {
 		return installerError(err)
